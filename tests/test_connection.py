85e45eaccbb90500ce110506a9b9801ca523163e
Ryan Roemmich <ryan@weareinstrument.com>
2011-07-28 22:05:41 -0700

    add test for get_libraries

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -9,6 +9,7 @@ import datetime, md5
 import harvestmedia.api.exceptions
 import harvestmedia.api.config
 import harvestmedia.api.client
+from harvestmedia.api.library import Library
 
 api_key = '12345'
 webservice_url = 'https://service.harvestmedia.net/HMP-WS.svc'
@@ -38,3 +39,28 @@ def test_get_service_token(UrlOpenMock):
     assert client.service_token == test_token
     assert client.service_token_expires == expiry
 
+@mock.patch('harvestmedia.api.client.urlopen')
+def test_get_libraries(UrlOpenMock):
+    u = UrlOpenMock()
+    expiry = datetime.datetime.today().isoformat()
+    test_token = md5.md5(expiry).hexdigest() # generate an md5 from the date for testing
+    u.read.return_value = '<?xml version="1.0" encoding="utf-8"?><responseservicetoken><token value="%s" expiry="%s"/></responseservicetoken>' % (test_token, expiry)
+    
+    hmconfig = harvestmedia.api.config.Config()
+    hmconfig.api_key = api_key
+    hmconfig.webservice_url = webservice_url
+    client = harvestmedia.api.client.Client()
+    u.read.return_value = """
+        <ResponseLibraries>
+            <libraries> 
+                <library id="abc123" name="VIDEOHELPER" detail="Library description" />
+                <library id="abc125" name="MODULES" detail="Library description" />
+            </libraries>
+        </ResponseLibraries>"""
+
+    libraries = Library.get_libraries()
+    assert isinstance(libraries, list)
+
+    library = libraries[0]
+    assert library.id == 'abc123'
+    assert library.name == 'VIDEOHELPER'
