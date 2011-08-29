5030daa4363434b5421bab41c6ac5635782e8ab9
Ryan Roemmich <ryan@weareinstrument.com>
2011-08-29 08:59:28 -0700

    add remote tests

---
+++ remote_tests/hmtest.py
@@ -0,0 +1,25 @@
+import harvestmedia
+from harvestmedia.api.config import Config
+from harvestmedia.api.library import Library
+
+#dreload(harvestmedia.api.config)
+#dreload(harvestmedia.api.client)
+#dreload(harvestmedia.api.library)
+#dreload(harvestmedia.api.album)
+
+api_key = 'e1d5d645d2d984e499e816a7a314dfbd610149f124c3373455c37ad75ab3ffccf444a04a10953b62'
+webservice_url = 'https://service.harvestmedia.net/HMP-WS.svc'
+
+hmconfig = Config()
+hmconfig.api_key = api_key
+hmconfig.webservice_url = webservice_url
+libraries = Library.get_libraries()
+
+for library in libraries:
+    albums = library.get_albums()
+
+for album in albums:
+    tracks = album.get_tracks()
+
+for track in tracks:
+    print track.name
---
+++ remote_tests/playlisttest.py
@@ -0,0 +1,15 @@
+from harvestmedia.api.config import Config
+from harvestmedia.api.client import Client
+from harvestmedia.api.playlist import Playlist
+
+hmconfig = Config()
+hmconfig.api_key = 'e1d5d645d2d984e499e816a7a314dfbd610149f124c3373455c37ad75ab3ffccf444a04a10953b62'
+hmconfig.api_key = '799b25abdab989c1b1949063476b910954a8a9e74d064f3119f14ef98736a840b058bf34cbec5972'
+hmconfig.webservice_url = 'http://betaservice.harvestmedia.net/HMP-WS.svc'
+hmconfig.debug = True
+
+
+playlist = Playlist()
+playlist.member_id = '46d6bb0b1a2aafe7'
+playlist.name = 'test playlist'
+playlist.create()
