58d6e181a5f789f92d9c39cd0a641d1da23c5642
Ryan Roemmich <ryan@weareinstrument.com>
2011-08-31 17:24:29 -0700

    dev/prod toggle

--- remote_tests/playlisttest.py
+++ remote_tests/playlisttest.py
@@ -6,8 +6,11 @@ from harvestmedia.api.track import Track
 
 hmconfig = Config()
 hmconfig.api_key = 'e1d5d645d2d984e499e816a7a314dfbd610149f124c3373455c37ad75ab3ffccf444a04a10953b62'
-hmconfig.api_key = '799b25abdab989c1b1949063476b910954a8a9e74d064f3119f14ef98736a840b058bf34cbec5972'
-hmconfig.webservice_url = 'http://betaservice.harvestmedia.net/HMP-WS.svc'
+hmconfig.webservice_url = 'https://service.harvestmedia.net/HMP-WS.svc'
+
+
+#hmconfig.api_key = '799b25abdab989c1b1949063476b910954a8a9e74d064f3119f14ef98736a840b058bf34cbec5972'
+#hmconfig.webservice_url = 'http://betaservice.harvestmedia.net/HMP-WS.svc'
 hmconfig.debug = True
 
 member = Member.get_by_id('46d6bb0b1a2aafe7')
