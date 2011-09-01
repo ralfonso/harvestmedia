f62a35e45b7fcd72366b3e02dbd9e79a68828cb2
Ryan Roemmich <ryan@weareinstrument.com>
2011-09-01 14:45:04 -0700

    decode weird windows utf-16 format

--- harvestmedia/api/client.py
+++ harvestmedia/api/client.py
@@ -68,7 +68,7 @@ class Client(object):
         xml_doc_str = response.read()
 
         if self.debug:
-            logger.debug("server response: " + xml_doc_str)
+            logger.debug("server response: " + xml_doc_str.decode('utf_16_le'))
 
         try:
             root = ET.fromstring(xml_doc_str)
