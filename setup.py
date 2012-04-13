edf9eb687a89a32e9951a59a6b4de8e20cab32ca
Ryan Roemmich <ryan@weareinstrument.com>
2012-04-13 10:39:34 -0700

    add url

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
       packages=find_packages('.'),
       author='Ryan Roemmich',
       author_email='ryan@roemmich.org',
+      url='https://github.com/ralfonso/harvestmedia',
       description='An interface for the Harvest Media API (http://www.harvestmedia.net/)',
       install_requires=['httplib2', 'cElementTree', 'pytz', 'iso8601', ],
       keywords='Harvest HarvestMedia API Media Music',
