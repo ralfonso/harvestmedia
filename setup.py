22f4d262d61e0698ceccfa728db018fdb850cf25
Ryan Roemmich <ryan@weareinstrument.com>
2012-04-13 10:29:16 -0700

    update setup.py for 0.2

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
       name='harvestmedia',
-      version='0.0.1',
+      version='0.2',
       packages=find_packages('.'),
       author='Ryan Roemmich',
       author_email='ryan@roemmich.org',
-      description='Wrapper for the Harvest Media XML API',
+      description='An interface for the Harvest Media API (http://www.harvestmedia.net/)',
       install_requires=['httplib2', 'cElementTree', 'pytz', 'iso8601', ],
       keywords='Harvest HarvestMedia API Media Music',
 )
