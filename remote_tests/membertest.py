33f8ae1dccb6acab927c6e750d3e1aba844f6423
eric feigner <eric@weareinstrument.com>
2012-10-05 10:29:04 -0700

    cleaning up remote tests

--- remote_tests/membertest.py
+++ remote_tests/membertest.py
@@ -2,26 +2,23 @@ from harvestmedia.api.config import Config
 from harvestmedia.api.client import Client
 from harvestmedia.api.member import Member
 
-hmconfig = Config()
-hmconfig.api_key = 'e1d5d645d2d984e499e816a7a314dfbd610149f124c3373455c37ad75ab3ffccf444a04a10953b62'
-hmconfig.api_key = '799b25abdab989c1b1949063476b910954a8a9e74d064f3119f14ef98736a840b058bf34cbec5972'
-hmconfig.webservice_url = 'http://betaservice.harvestmedia.net/HMP-WS.svc'
-hmconfig.debug = True
+api_key = 'e1d5d645d2d984e499e816a7a314dfbd610149f124c3373455c37ad75ab3ffccf444a04a10953b62'
+webservice_url = 'https://service.harvestmedia.net/HMP-WS.svc'
 
-client = Client()
+client = Client(api_key=api_key, debug_level='DEBUG')
 
-#member = Member()
+#member = Member(client)
 #member.username = 'test123'
 #member.email = 'r@weareinstrument.com'
 #member.firstname = 'Ryan1'
 #member.lastname = 'Roemmich1'
 #member.create()
 
-#member = Member()
+#member = Member(client)
 #member.send_password('test')
 
-member = Member()
-authenticated = member.authenticate('test', 'test')
+member = Member(client)
+authenticated = member.authenticate('test', 'test', client)
 print authenticated
 
 playlists = member.get_playlists()
--- remote_tests/playlisttest.py
+++ remote_tests/playlisttest.py
@@ -4,16 +4,12 @@ from harvestmedia.api.playlist import Playlist
 from harvestmedia.api.member import Member
 from harvestmedia.api.track import Track
 
-hmconfig = Config()
-hmconfig.api_key = 'e1d5d645d2d984e499e816a7a314dfbd610149f124c3373455c37ad75ab3ffccf444a04a10953b62'
-hmconfig.webservice_url = 'https://service.harvestmedia.net/HMP-WS.svc'
+api_key = 'e1d5d645d2d984e499e816a7a314dfbd610149f124c3373455c37ad75ab3ffccf444a04a10953b62'
+webservice_url = 'https://service.harvestmedia.net/HMP-WS.svc'
 
+client = Client(api_key=api_key, debug_level='DEBUG')
 
-#hmconfig.api_key = '799b25abdab989c1b1949063476b910954a8a9e74d064f3119f14ef98736a840b058bf34cbec5972'
-#hmconfig.webservice_url = 'http://betaservice.harvestmedia.net/HMP-WS.svc'
-hmconfig.debug = True
-
-member = Member.get_by_id('46d6bb0b1a2aafe7')
+member = Member.query.get_by_id('46d6bb0b1a2aafe7', client)
 playlists = member.get_playlists()
 
 #playlist = Playlist()
